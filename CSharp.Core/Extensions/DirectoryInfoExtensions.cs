d2842509cf677c03eb37916af5ed3519d7dcd7a0
Dean Edis <dean.edis@gmail.com>
2024-10-13 17:42:31 +0100

    Feature: Directory + path extension resolution.

--- CSharp.Core/Extensions/DirectoryInfoExtensions.cs
+++ CSharp.Core/Extensions/DirectoryInfoExtensions.cs
@@ -47,4 +47,40 @@ public static class DirectoryInfoExtensions
 
     public static DirectoryInfo Clone(this DirectoryInfo info) =>
         info == null ? null : new DirectoryInfo(info.FullName);
+
+    /// <summary>
+    /// Resolves a path relative to the current DirectoryInfo (and maybe including file wildcard).
+    /// </summary>
+    public static string Resolve(this DirectoryInfo info, string morePath)
+    {
+        if (string.IsNullOrEmpty(morePath))
+            return info.FullName;
+        
+        if (morePath.StartsWith('~'))
+        {
+            info = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile).ToDir();
+            morePath = morePath[1..].TrimStart('\\', '/');
+        }
+
+        while (morePath.StartsWith("..") && info.Parent != null)
+        {
+            info = info.Parent;
+            morePath = morePath[2..].TrimStart('\\', '/');
+        }
+        
+        // Remove any file filter.
+        string result;
+        var starIndex = morePath.IndexOf('*');
+        if (starIndex >= 0)
+        {
+            result = Path.GetFullPath(Path.Combine(info.FullName, morePath.Substring(0, starIndex)));
+            result = Path.Combine(result, morePath.Substring(starIndex));
+        }
+        else
+        {
+            result = Path.GetFullPath(Path.Combine(info.FullName, morePath));
+        }
+        
+        return result;
+    }
 }
