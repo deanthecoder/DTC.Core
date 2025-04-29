dc0a92f12526ff392dd6f552241bfe7615fb7cd8
dedis <dean.edis@globalgraphics.com>
2025-04-29 14:21:10 +0100

    Feature: Add ProgressToken

---
+++ CSharp.Core/ProgressToken.cs
@@ -0,0 +1,36 @@
+// Code authored by Dean Edis (DeanTheCoder).
+// Anyone is free to copy, modify, use, compile, or distribute this software,
+// either in source code form or as a compiled binary, for any non-commercial
+//  purpose.
+// 
+// If you modify the code, please retain this copyright header,
+// and consider contributing back to the repository or letting us know
+// about your modifications. Your contributions are valued!
+// 
+// THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND.
+
+using System;
+using CSharp.Core.Extensions;
+
+namespace CSharp.Core;
+
+public class ProgressToken
+{
+    private double m_progress;
+    
+    public event EventHandler ProgressUpdated;
+    
+    public bool CancelRequested { get; set; }
+
+    public double Progress
+    {
+        get => m_progress;
+        set
+        {
+            if (Math.Abs(m_progress - value) < 0.01)
+                return;
+            m_progress = value.Clamp(0.0, 1.0);
+            ProgressUpdated?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
